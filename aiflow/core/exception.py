9af59bcce8594208cab3c399df3838e430555218
Fernando Celmer <email@fernandocelmer.com>
2026-08-05 14:26:21 -0300

    ⚙️ FEATURE: Add NotProviderInstance exception

---
+++ aiflow/core/exception.py
@@ -0,0 +1,11 @@
+"""Exception module"""
+
+MESSAGE_NOT_PROVIDER_INSTANCE = (
+    "Problem validating the '{name}' object type; this is not an "
+    "instance of aiflow.abc.Provider"
+)
+
+
+class NotProviderInstance(Exception):
+    def __init__(self, name: str):
+        super().__init__(MESSAGE_NOT_PROVIDER_INSTANCE.format(name=name))
